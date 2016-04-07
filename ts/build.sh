fb8ba87c20be49c795cf1bdc85d66302e615bb8f
Simon Heath <sheath@andrew.cmu.edu>
2016-04-07 13:06:28 -0400

    Javascript build systems are downright criminal.
    It's all Node's fault.

--- ts/build.sh
+++ ts/build.sh
@@ -5,7 +5,13 @@
 # Ugh.
 # Redo this to use npm or browserify?
 echo Building typescript...
-tsc -t es6 astrogears.ts
+tsc -t es6 astrogears.ts --out astrogears-es6.js
 echo Turning fancy-ass ES2015 javascript to old-ass javascript... 
-babel astrogears.js -o astrogears-built.js
-echo "Open astrogears.html in a browser to see it work."
+babel astrogears-es6.js -o astrogears-built.js --presets es2015
+#browserify astrogears.ts -o astrogears.js -p [ tsify --target=es6 ] -t [ babelify --presets [ es6 ] ]
+
+# AAAAARUGHFJDSKFJDLSAFJSL
+## How the fuck do we make browserify do this as a single step???
+#browserify astrogears.ts -o astrogears-es6.js -p [ tsify --target=es6 ] 
+#browserify astrogears-es6.js -o bundle.js -t [ babelify --presets [ es2015 ] ]
+#echo "Open astrogears.html in a browser to see it work."
--- ts/setup.sh
+++ ts/setup.sh
@@ -10,7 +10,7 @@ sudo apt-get install -y nodejs
 sudo npm update -g npm
 
 # Install npm deps
-sudo npm install -g typescript
-sudo npm install -g babel-cli babel-preset-stage-0 babel-preset-es2015 babel-polyfill
-sudo npm install -g browserify tsify babelify
+sudo npm install -g typescript babel-cli browserify
+npm install babel-preset-stage-0 babel-preset-es2015 babel-polyfill
+npm install tsify babelify
 browserify astrogears.js -o astrogears-built.js -t babelify
