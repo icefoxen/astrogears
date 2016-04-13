284c127d53a6d84ad388339b4fa2a34d2dd6b071
Simon Heath <icefoxen@gmail.com>
2016-04-12 23:07:40 -0400

    Updated build stuff, updated stylesheet

--- ts/setup.sh
+++ ts/setup.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 # Crude setup for Debian systems
 
+sudo apt-get install npm 
+
 # Install node.js
 
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-sudo apt-get install -y nodejs
+#curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+#sudo apt-get install -y nodejs
 
 # Update npm
 sudo npm update -g npm
 
 # Install npm deps
-sudo npm install -g typescript babel-cli browserify
-npm install babel-preset-stage-0 babel-preset-es2015 babel-polyfill
-npm install tsify babelify
-browserify astrogears.js -o astrogears-built.js -t babelify
+npm install babel-preset-es2015 babel-polyfill typescript babel-cli
+#npm install browserify tsify babelify
