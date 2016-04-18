9c42d9223130a33d2eda689b8c7a2c7db965a73e
Simon Heath <icefoxen@gmail.com>
2016-04-18 19:21:49 -0400

    Starting to flesh out the algorithm itself.

---
+++ Astrogears-GUI/Astrogears.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace AstrogearsGUI
+{
+	public class Result {
+		public int[] Wheels;
+		public int[] Pinions;
+		public double Ratio;
+		public double Error;
+
+		public Result(int gearTrainLength, double ratio, double error) {
+			Wheels = new int[gearTrainLength];
+			Pinions = new int[gearTrainLength];
+			Ratio = ratio;
+			Error = error;
+		}
+
+		public override string ToString() {
+			var sb = new StringBuilder ();
+			sb.Append ("Wheel/pinion ");
+			for (var i = 0; i < Wheels.Length; i++) {
+				sb.AppendFormat ("{0}/{1} ", Wheels[i], Pinions[i]);
+			}
+			sb.AppendFormat (" Gear ratio: {0}, Error {1}", Ratio, Error);
+			return sb.ToString ();
+		}
+	}
+
+	public class RunState {
+		public double Target;
+		public double Error;
+		public List<Result> Results;
+
+		public RunState(int gearsize, double target, double error) {
+			this.Target = target;
+			this.Error = error;
+			this.Results = new List<Result> ();
+		}
+
+		public void AddResult(Result r) {
+			Results.Add (r);
+		}
+	}
+
+	public static class Astrogears
+	{
+		public static void FindRatios1(double ratio, int minTeeth, int maxTeeth, RunState state) {
+			for (int wheel = minTeeth; wheel <= maxTeeth; wheel++) {
+				for (int pinion = minTeeth; pinion <= maxTeeth; pinion++) {
+					var r = (double)wheel / (double)pinion;
+					var result = ratio * r;
+					var error = Math.Abs(state.Target - result);
+					if (error < state.Error) {
+						var theResult = new Result(0, result, error);
+						state.AddResult (theResult);
+					}
+				}
+			}
+		}
+	}
+}
+
--- Astrogears-GUI/Program.cs
+++ Astrogears-GUI/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using Gtk;
+using PeterO.Numbers;
 
 namespace AstrogearsGUI
 {
@@ -10,7 +12,21 @@ namespace AstrogearsGUI
 			Application.Init ();
 			MainWindow win = new MainWindow ();
 			win.Show ();
+			Run ();
 			Application.Run ();
 		}
+
+		public static void Run() {
+			var t = new Stopwatch ();
+			var rs = new RunState (0, 1.002737909350795, 0.000003);
+			t.Start ();
+			Astrogears.FindRatios1(1, 5, 500, rs);
+			t.Stop ();
+			Console.WriteLine ("Found {0} results in {1} seconds", rs.Results.Count, t.ElapsedMilliseconds / 1000.0);
+			foreach (var r in rs.Results) {
+				Console.WriteLine (r);
+			}
+		}
 	}
+
 }
