bbc9e8f55e73c88daf444eb0738d2b92ef55065c
Simon Heath <icefoxen@gmail.com>
2016-04-18 18:22:26 -0400

    Okay let's make this a real thing
    Since we have an arbitrary precision number library... why not?  :D

---
+++ Astrogears-GUI/Tests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace AstrogearsGUI
+{
+	[TestFixture ()]
+	public class NUnitTestClass
+	{
+		[Test ()]
+		public void TrivialTestCase ()
+		{
+			Assert.IsTrue (true);
+		}
+	}
+}
+
--- fs/main.fs
+++ fs/main.fs
@@ -4,20 +4,22 @@ let error_limit = 0.000003
 
 
 let find_ratios_explicit_loops2 (min_teeth:int) (max_teeth:int) =
-    for wheel1 in min_teeth..max_teeth+1 do
-        for wheel2 in min_teeth..max_teeth+1 do
-            for pinion1 in min_teeth..max_teeth+1 do
-                for pinion2 in min_teeth..max_teeth+1 do
-                    let wheel1d = double wheel1
-                    let wheel2d = double wheel2
-                    let pinion1d = double pinion1
-                    let pinion2d = double pinion2
+    async {
+        for wheel1 in min_teeth..max_teeth+1 do
+            for wheel2 in min_teeth..max_teeth+1 do
+                for pinion1 in min_teeth..max_teeth+1 do
+                    for pinion2 in min_teeth..max_teeth+1 do
+                        let wheel1d = double wheel1
+                        let wheel2d = double wheel2
+                        let pinion1d = double pinion1
+                        let pinion2d = double pinion2
 
-                    let gear_ratio = (wheel1d/pinion1d) * (wheel2d/pinion2d)
-                    let error = abs (gear_ratio - target_gear_range)
-                    if error < error_limit then
-                        printf "Good gear ratio found: (%A,%A,%A,%A), ratio %A +/- %A\n" wheel1 pinion1 wheel2 pinion2 gear_ratio error
+                        let gear_ratio = (wheel1d/pinion1d) * (wheel2d/pinion2d)
+                        let error = abs (gear_ratio - target_gear_range)
+                        if error < error_limit then
+                            yield sprintf "Good gear ratio found: (%A,%A,%A,%A), ratio %A +/- %A\n" wheel1 pinion1 wheel2 pinion2 gear_ratio error
+                            //printf "Good gear ratio found: (%A,%A,%A,%A), ratio %A +/- %A\n" wheel1 pinion1 wheel2 pinion2 gear_ratio error
+    }
 
-
-find_ratios_explicit_loops2 5 100
-
+for x in find_ratios_explicit_loops2 5 100 do
+    printf "%s" x
