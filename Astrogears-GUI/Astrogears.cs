330c8f72a1ce7495dceab676a0b913eb7623f2d0
Simon Heath <icefoxen@gmail.com>
2016-04-18 20:34:36 -0400

    Making continuation-passing ratio thingy.
    It's a start at least, though it doesn't really work right yet.

--- Astrogears-GUI/Astrogears.cs
+++ Astrogears-GUI/Astrogears.cs
@@ -31,12 +31,16 @@ namespace AstrogearsGUI
 	public class RunState {
 		public double Target;
 		public double Error;
+		public int MinTeeth;
+		public int MaxTeeth;
 		public List<Result> Results;
 
-		public RunState(int gearsize, double target, double error) {
+		public RunState(int gearsize, double target, double error, int minTeeth, int maxTeeth) {
 			this.Target = target;
 			this.Error = error;
 			this.Results = new List<Result> ();
+			this.MinTeeth = minTeeth;
+			this.MaxTeeth = maxTeeth;
 		}
 
 		public void AddResult(Result r) {
@@ -46,6 +50,49 @@ namespace AstrogearsGUI
 
 	public static class Astrogears
 	{
+		public static void FindRatiosContN(double ratio, RunState state, int n) {
+			var conts = new ContinuationThing[n];
+			for (int i = 0; i < n - 1; i++) {
+				conts [i] = FindRatiosContN;
+			}
+			conts [n - 1] = FindRatiosCont1;
+			FindRatiosContN (ratio, state, conts, 0);
+		}
+		delegate void ContinuationThing(double ratio, RunState state, ContinuationThing[] conts, int n);
+		static void FindRatiosContN(double ratio, RunState state, ContinuationThing[] conts, int n) {
+			var cont = conts [n];
+			for(var wheel = state.MinTeeth; wheel <= state.MaxTeeth; wheel++) {
+				for (var pinion = state.MinTeeth; pinion <= state.MaxTeeth; pinion++) {
+					var result = ratio * ((double)wheel / (double)pinion);
+					cont (result, state, conts, n + 1);
+				}
+			}
+		}
+
+		static void FindRatiosCont1(double ratio, RunState state, ContinuationThing[] conts, int n) {
+			for(var wheel = state.MinTeeth; wheel <= state.MaxTeeth; wheel++) {
+				for (var pinion = state.MinTeeth; pinion <= state.MaxTeeth; pinion++) {
+					var result = ratio * ((double)wheel / (double)pinion);
+					var error = Math.Abs(state.Target - result);
+					//Console.WriteLine ("Argl, {0}, {1}", state.MinTeeth, state.MaxTeeth);
+					//Console.WriteLine ("Foobaz? {0} {1} {2}", state.Target, result, error);
+					if (error < state.Error) {
+						var theResult = new Result(0, result, error);
+						state.AddResult (theResult);
+					}
+				}
+			}
+		}
+
+		public static void FindRatios2(double ratio, int minTeeth, int maxTeeth, RunState state) {
+			for (int wheel = minTeeth; wheel <= maxTeeth; wheel++) {
+				for (int pinion = minTeeth; pinion <= maxTeeth; pinion++) {
+					var r = (double)wheel / (double)pinion;
+					var result = ratio * r;
+					FindRatios1 (result, minTeeth, maxTeeth, state);
+				}
+			}
+		}
 		public static void FindRatios1(double ratio, int minTeeth, int maxTeeth, RunState state) {
 			for (int wheel = minTeeth; wheel <= maxTeeth; wheel++) {
 				for (int pinion = minTeeth; pinion <= maxTeeth; pinion++) {
--- Astrogears-GUI/Program.cs
+++ Astrogears-GUI/Program.cs
@@ -18,13 +18,21 @@ namespace AstrogearsGUI
 
 		public static void Run() {
 			var t = new Stopwatch ();
-			var rs = new RunState (0, 1.002737909350795, 0.000003);
+			var rs = new RunState (0, 1.002737909350795, 0.000003, 5, 100);
 			t.Start ();
-			Astrogears.FindRatios1(1, 5, 500, rs);
+			Astrogears.FindRatios2(1, 5, 100, rs);
 			t.Stop ();
 			Console.WriteLine ("Found {0} results in {1} seconds", rs.Results.Count, t.ElapsedMilliseconds / 1000.0);
+
+
+			t.Reset ();
+			rs = new RunState (0, 1.002737909350795, 0.000003, 5, 100);
+			t.Start ();
+			Astrogears.FindRatiosContN(1, rs, 2);
+			t.Stop();
+			Console.WriteLine ("Found {0} results in {1} seconds", rs.Results.Count, t.ElapsedMilliseconds / 1000.0);
 			foreach (var r in rs.Results) {
-				Console.WriteLine (r);
+				//Console.WriteLine (r);
 			}
 		}
 	}
