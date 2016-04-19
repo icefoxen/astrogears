238f9b3a9963da2c2b3016d36c8d5c7b4fd33cbe
Simon Heath <icefoxen@gmail.com>
2016-04-18 23:38:46 -0400

    Messed around with f# async stuff
    Apparently not designed for heavy computation...

--- fs/main.fs
+++ fs/main.fs
@@ -22,7 +22,7 @@ let find_ratios_explicit_loops2 (min_teeth:int) (max_teeth:int) =
                         printf "Good gear ratio found: (%A,%A,%A,%A), ratio %A +/- %A\n" wheel1 pinion1 wheel2 pinion2 gear_ratio error
 
 let find_ratios_explicit_loops2_tasks (min_teeth:int) (max_teeth:int) =
-    let tasks = new List<Task>()
+    let tasks = new List<Task>(max_teeth - min_teeth + 1)
     for wheel1 in min_teeth..max_teeth+1 do
        let f () =
         for wheel2 in min_teeth..max_teeth+1 do
@@ -42,34 +42,7 @@ let find_ratios_explicit_loops2_tasks (min_teeth:int) (max_teeth:int) =
        tasks.Add(t)
     tasks
 
+let tasks = find_ratios_explicit_loops2_tasks 5 250
+printf "Number of tasks: %d\n" (tasks.Count)
+Task.WaitAll(tasks.ToArray())
 
-let find_ratios_explicit_loops2_async (min_teeth:int) (max_teeth:int) =
-  async {
-    for wheel1 in min_teeth..max_teeth+1 do
-        for wheel2 in min_teeth..max_teeth+1 do
-            for pinion1 in min_teeth..max_teeth+1 do
-                for pinion2 in min_teeth..max_teeth+1 do 
-                    let wheel1d = double wheel1
-                    let wheel2d = double wheel2
-                    let pinion1d = double pinion1
-                    let pinion2d = double pinion2
-
-                    let gear_ratio = (wheel1d/pinion1d) * (wheel2d/pinion2d)
-                    let error = abs (gear_ratio - target_gear_range)
-                    if error < error_limit then
-                        printf "Good gear ratio found: (%A,%A,%A,%A), ratio %A +/- %A\n" wheel1 pinion1 wheel2 pinion2 gear_ratio error
-     }
-
-
-//find_ratios_explicit_loops2 5 100
-//let tasks = find_ratios_explicit_loops2_tasks 5 150
-//printf "Number of tasks: %d\n" (tasks.Count)
-//Task.WaitAll(tasks.ToArray())
-
-find_ratios_explicit_loops2_async 5 100 |>
-Async.RunSynchronously
-
-//find_ratios_explicit_loops2_async 5 150 |>
-//Seq.map Async.StartAsTask |>
-//Seq.map (fun x -> x.Wait()) |>
-//printf "Foo? %A\n"
